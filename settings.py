a24136c7941038e79ebbe04f80474a61c6b4a372
linzerd <mooder@gmail.com>
2012-08-02 21:22:07 +0800

    add template

--- settings.py
+++ settings.py
@@ -65,7 +65,7 @@ STATIC_ROOT = os.path.join(ROOT_PATH,'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = ' '
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -12,10 +13,12 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+#    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.ADMIN_MEDIA_ROOT}),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     # Uncomment the next line to enable the admin:
-     url(r'^puzi/(?P<id>\d+)/$','puzi.views.show'),
+    url(r'^puzi/(?P<id>\d+)/$','puzi.views.show'),
+    url(r'^$','puzi.views.index'),
 
 
-
-     url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
