7ebd5866b479af2ec279f184be95d781580c495c
scotfu <scotfu@gmail.com>
2012-08-02 17:38:27 +0800

    keep initing

---
+++ like/models.py
@@ -0,0 +1,15 @@
+#coding=utf8
+from django.db import models
+from puzi.models import Puzi
+from django.contrib.auth.models import User
+
+class Like(models.Model):
+    puzi=models.ForeignKey(Puzi)
+    user=models.ForeignKey(User)
+    create_time=models.DateTimeField(auto_now_add=True)
+    
+    def __unicode__(self):
+        return self.puzi.name+':'+self.user.name
+    
+    class Meta:
+       verbose_name_plural=u'like'
